b3282f2e4816e57564c2b54e521c5de974d347e7
ayasaokamoto <s38090108y@gmail.com>
2024-04-22 15:33:56 +0900

    DBテーブル作成

---
+++ database/migrations/2024_04_22_042256_create_lists_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('color_id');
+            $table->string('contents', 255);
+            $table->tinyInteger('purchased')->comment('0: Not purchased, 1: Purchased');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lists');
+    }
+};
